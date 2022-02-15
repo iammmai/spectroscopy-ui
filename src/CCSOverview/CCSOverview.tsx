046c1e537d38cb7c99d9fbc29c40b4e3f2ebad72
iammmai <duongm279@gmail.com>
2022-02-15 20:25:54 +0100

    useMutation to create spectroscopy

--- src/CCSOverview/CCSOverview.tsx
+++ src/CCSOverview/CCSOverview.tsx
@@ -1,12 +1,15 @@
-import "./CCSOverview.css";
+import LoadingButton from "@mui/lab/LoadingButton";
+import { TextField } from "@mui/material";
+import { parser } from "@pseuco/ccs-interpreter";
 import { isEmpty } from "ramda";
+import React, { useReducer } from "react";
+import { useMutation } from "react-query";
 import { useNavigate } from "react-router-dom";
 
-import { Button, TextField } from "@mui/material";
-import { parser } from "@pseuco/ccs-interpreter";
-import React, { useReducer } from "react";
 import Header from "Header/Header";
 import api from "api";
+
+import "./CCSOverview.css";
 
 type ProcessId = "p1" | "p2";
 
@@ -59,6 +62,10 @@ const reducer = (
 
 function CCSOverview() {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const createSpectroscopyMutation = useMutation(
+    (data: { p1: String; p2: String }) => api.post("/", data)
+  );
+
   let navigate = useNavigate();
 
   const handleChange = (
@@ -90,18 +97,12 @@ function CCSOverview() {
   };
 
   const handleCompare = () => {
-    api
-      .post("/", {
-        p1: state.p1.ccs,
-        p2: state.p2.ccs,
-      })
-      .then((response) => {
-        navigate(`/${response.data._id}`);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    createSpectroscopyMutation.mutate({ p1: state.p1.ccs, p2: state.p2.ccs });
   };
+
+  if (createSpectroscopyMutation.isSuccess) {
+    navigate(`/${createSpectroscopyMutation.data.data._id}`);
+  }
 
   return (
     <div className="App">
@@ -138,7 +139,7 @@ function CCSOverview() {
             })}
           />
         </div>
-        <Button
+        <LoadingButton
           variant="contained"
           onClick={handleCompare}
           disabled={
@@ -147,9 +148,10 @@ function CCSOverview() {
             isEmpty(state.p1.ccs) ||
             isEmpty(state.p2.ccs)
           }
+          loading={createSpectroscopyMutation.isLoading}
         >
           Compare
-        </Button>
+        </LoadingButton>
       </div>
     </div>
   );
