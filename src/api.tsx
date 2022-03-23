4331d6c5d4fd6b51b1ec11695fbeea8a935948c0
iammmai <duongm279@gmail.com>
2022-03-23 11:20:22 +0100

    Fix base url

--- src/api.tsx
+++ src/api.tsx
@@ -1,11 +1,13 @@
 import axios from "axios";
+import "dotenv/config";
 
 const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
   withCredentials: true,
-  baseURL: process.env.API_BASE_URL || "http://localhost:8080/spectroscopy",
+  baseURL:
+    process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/spectroscopy",
   timeout: 10000,
 });
 
